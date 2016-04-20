5ddad9847fd095b7f48ff3e1d56c7770d0f1481e
Raymond D <raydunc15@gmail.com>
2016-04-20 16:48:04 -0600

    This should work now...

--- geometry_test.c
+++ geometry_test.c
@@ -146,8 +146,8 @@ START_TEST(test_2d_midpoint)
 END_TEST
 
 /*coord_2d_area_triangle Test*/
-START_TEST(test_2d_area_triangle){
-
+START_TEST(test_2d_area_triangle)
+{
     coord_2d_t a;
     coord_2d_t b;
     coord_2d_t c;
@@ -161,6 +161,7 @@ START_TEST(test_2d_area_triangle){
     ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0);
     
 }
+END_TEST
 
 
 /* coord_2d Test Suite */
@@ -181,7 +182,7 @@ Suite* coord_2d_suite(void)
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
     TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
-    tcase_add_test(tc_2d_area_triangle, tc_2d_area_triangle);
+    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle);
 
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
